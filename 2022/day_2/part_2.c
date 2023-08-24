ba6d9ad6719ca2f2ce3d0583bbc6ff4726cc1c8a
J Martin <jagm@amrna.net>
2023-08-24 16:56:13 +0200

    add day 2, part 2

---
+++ 2022/day_2/part_2.c
@@ -0,0 +1,78 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+
+typedef struct {
+    char *key;
+    int score;
+} round;
+
+ssize_t getline(char **restrict, size_t *restrict, FILE *restrict);
+int score_round(round *, size_t, const char *);
+
+int main(void) {
+    FILE *fp = fopen("input.txt", "r");
+
+    if (fp == NULL) {
+        perror("fopen");
+        return -1;
+    }
+
+    char *line = NULL;
+    size_t len = 0;
+    round rounds[] = {
+        // X: lose, Y: draw, Z: win
+        /*
+        A Y
+        B X
+        C Z
+        */
+        {"A X", 3 + 0},
+        {"A Y", 1 + 3},
+        {"A Z", 2 + 6},
+        {"B X", 1 + 0},
+        {"B Y", 2 + 3},
+        {"B Z", 3 + 6},
+        {"C X", 2 + 0},
+        {"C Y", 3 + 3},
+        {"C Z", 1 + 6},
+    };
+    size_t nrounds = sizeof(rounds) / sizeof(round);
+    int score = 0;
+    int s = 0;
+
+    while (getline(&line, &len, fp) != EOF) {
+        if (line[0] == '\0')
+            continue;
+
+        line[strlen(line) - 1] = '\0';
+        s = score_round(rounds, nrounds, line);
+
+        if (s > 0 && score > INT_MAX - s) {
+            return 1;
+        }
+
+        score += s;
+    }
+
+    free(line);
+    fclose(fp);
+
+    printf("score: %d\n", score);
+
+    return 0;
+}
+
+int score_round(round *rounds, size_t size, const char *key) {
+    int score = 0;
+
+    for (int i = 0; i < size; i++)
+        if (strcmp(rounds[i].key, key) == 0) {
+            score = rounds[i].score;
+            break;
+        }
+
+    return score;
+}
